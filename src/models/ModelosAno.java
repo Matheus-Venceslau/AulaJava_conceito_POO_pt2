5d455c05c6e277b93244c0219d42e8468e648e7d
Matheus Venceslau <matheushlvenceslau@gmail.com>
2025-04-05 12:38:25 -0300

    Encapsulamento

---
+++ src/models/ModelosAno.java
@@ -0,0 +1,13 @@
+package models;
+
+// Conceito de polimorfismo
+// - Permite que um método tenha comportamentos diferentes dependendo da classe.
+//- Permite criar novas classes baseadas em uma classe existente.
+//- A classe filha herda os atributos e métodos da classe pai.
+
+public class Fabricante {
+    public void VeiculoDoAno(){
+        System.out.println("Carro de Corrida");
+    }
+
+}
